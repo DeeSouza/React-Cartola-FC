d50793a2597c2f6ec6ace408e6a99d7718b59567
Diego Souza <dee-souza@hotmail.com>
2019-10-15 17:02:40 -0300

    :art: Layout menu open and button

--- src/pages/Layouts/Frontend/index.js
+++ src/pages/Layouts/Frontend/index.js
@@ -1,15 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { WrapperLayout, MainWrapper, HeaderMenu, OpenMenu } from './styles';
+import {
+	FaShoppingBasket,
+	FaUsers,
+	FaSignOutAlt,
+	FaTrophy,
+	FaTimes,
+} from 'react-icons/fa';
+import {
+	WrapperLayout,
+	MainWrapper,
+	HeaderMenu,
+	OpenMenu,
+	Menu,
+	CloseMenu,
+} from './styles';
 
 export default function FrontendLayout({ children }) {
+	const [open, setOpen] = useState(false);
+
+	function openMenu() {
+		setOpen(!open);
+	}
+
+	function closeMenu() {
+		setOpen(!open);
+	}
+
 	return (
 		<WrapperLayout>
 			<HeaderMenu>
-				<OpenMenu>
-					<span></span>
+				<OpenMenu onClick={openMenu}>
+					<span />
 				</OpenMenu>
 			</HeaderMenu>
+
+			<Menu open={open}>
+				<ul>
+					<li>
+						<a href="#">
+							<FaShoppingBasket size={15} color="#FFF" />
+							<div>MERCADO</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<FaUsers size={15} color="#FFF" />
+							<div>TIME</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<FaTrophy size={15} color="#FFF" />
+							<div>LIGAS</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<FaSignOutAlt size={15} color="#FFF" />
+							<div>SAIR</div>
+						</a>
+					</li>
+				</ul>
+
+				<CloseMenu onClick={closeMenu}>
+					<FaTimes size={25} color="#00b3a5" />
+				</CloseMenu>
+			</Menu>
+
 			<MainWrapper>{children}</MainWrapper>
 		</WrapperLayout>
 	);
--- src/pages/Layouts/Frontend/styles.js
+++ src/pages/Layouts/Frontend/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import settings from '../../../styles/variables';
 
 export const WrapperLayout = styled.div`
@@ -7,7 +7,7 @@ export const WrapperLayout = styled.div`
 	justify-content: center;
 	align-items: center;
 	min-height: 100%;
-	background: #fff;
+	background: whitesmoke;
 `;
 
 export const MainWrapper = styled.main`
@@ -25,7 +25,11 @@ export const HeaderMenu = styled.div`
 	top: 0;
 	left: 0;
 	height: 50px;
-	background: linear-gradient(90deg, #00b3a5, #ffca4f);
+	background: linear-gradient(
+		90deg,
+		${settings.linearGradientPrimary},
+		${settings.linearGradiendSecondary}
+	);
 	box-shadow: #565656 0px 0px 6px 0px;
 `;
 
@@ -65,4 +69,62 @@ export const OpenMenu = styled.button`
 	&:after {
 		top: 22px;
 	}
+`;
+
+export const Menu = styled.nav`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: ${settings.linearGradientPrimary};
+	display: flex;
+	justify-content: left;
+	align-items: center;
+	padding-left: 70px;
+	transform: ${props => (props.open ? 'translateX(0%)' : 'translateX(-100%)')};
+	transition: all 0.25s ease-out;
+
+	&:after{
+		content: "";
+		height: 120%;
+		background-color: #FFF;
+		top: 0;
+		bottom: 0;
+		right: 0;
+		margin: auto;
+		position: absolute;
+		width: 100px;
+		border-top-left-radius: 50%;
+    	border-bottom-left-radius: 50%;
+	}
+
+	li {
+		margin: 40px auto;
+
+		a {
+			color: #fff;
+			font-family: "${settings.fontRoboto}";
+			font-weight: 400;
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+
+			svg {
+				margin-right: 10px;
+			}
+
+			div {
+				font-size: 16px;
+			}
+		}
+	}
+`;
+
+export const CloseMenu = styled.div`
+	content: '';
+	position: absolute;
+	top: 20px;
+	right: 20px;
+	z-index: 1;
 `;
