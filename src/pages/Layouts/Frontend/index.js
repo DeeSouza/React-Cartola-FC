1dcbde78c8ca651f478b24530e0b3f81ec59d33e
Diego Souza <dee-souza@hotmail.com>
2019-10-15 16:17:17 -0300

    :art: Menu header and open button menu

--- src/pages/Layouts/Frontend/index.js
+++ src/pages/Layouts/Frontend/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { WrapperLayout, MainWrapper } from './styles';
+import { WrapperLayout, MainWrapper, HeaderMenu, OpenMenu } from './styles';
 
 export default function FrontendLayout({ children }) {
 	return (
 		<WrapperLayout>
+			<HeaderMenu>
+				<OpenMenu>
+					<span></span>
+				</OpenMenu>
+			</HeaderMenu>
 			<MainWrapper>{children}</MainWrapper>
 		</WrapperLayout>
 	);
--- src/pages/Layouts/Frontend/styles.js
+++ src/pages/Layouts/Frontend/styles.js
@@ -7,15 +7,62 @@ export const WrapperLayout = styled.div`
 	justify-content: center;
 	align-items: center;
 	min-height: 100%;
-	background: linear-gradient(
-		360deg,
-		${settings.linearGradientPrimary},
-		${settings.linearGradiendSecondary}
-	);
+	background: #fff;
 `;
 
 export const MainWrapper = styled.main`
 	width: 90%;
 	max-width: 315px;
 	text-align: center;
+`;
+
+export const HeaderMenu = styled.div`
+	position: fixed;
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	width: 100%;
+	top: 0;
+	left: 0;
+	height: 50px;
+	background: linear-gradient(90deg, #00b3a5, #ffca4f);
+	box-shadow: #565656 0px 0px 6px 0px;
+`;
+
+export const OpenMenu = styled.button`
+	display: block;
+	width: 25px;
+	height: 35px;
+	background-color: transparent;
+	border: none;
+	display: flex;
+	justify-content: center;
+	margin-left: 15px;
+	position: relative;
+
+	span {
+		display: block;
+		height: 2px;
+		width: 100%;
+		background-color: #fff;
+		margin: auto;
+	}
+
+	&:before,
+	&:after {
+		content: '';
+		position: absolute;
+		height: 2px;
+		background-color: #fff;
+		width: 80%;
+		left: 0;
+	}
+
+	&:before {
+		top: 10px;
+	}
+
+	&:after {
+		top: 22px;
+	}
 `;
